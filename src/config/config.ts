eb4b9fb61e52486780ec1ee1c94045161c1acb07
bankmotion <harris5555555555555@gmail.com>
2024-12-15 09:43:23 +0900

    FIX: config file

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 export const APIUrl = "https://api.balldontlie.io/nfl/v1/players";
 export const Port = 5000;
-export const BackendURL = `http://localhost:${Port}`;
-export const FrontendURL = `http://localhost:3000`;
+export const IPAddress =
+  process.env.LIVE_MODE === "true"
+    ? "http://147.182.188.81"
+    : "http://localhost";
+export const BackendURL = `${IPAddress}:${Port}`;
+export const FrontendURL = `${IPAddress}:3000`;

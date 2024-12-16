1f662aec60dafb1d9d68cb4eeee7372b3bbdc37b
bankmotion <harris5555555555555@gmail.com>
2024-12-16 20:41:34 +0900

    FIX

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,8 +5,11 @@ export const APIUrl = "https://api.balldontlie.io/nfl/v1/players";
 export const Port = 5000;
 export const IPAddress =
   process.env.LIVE_MODE === "true"
-    ? "https://backend.alumnigrid.com"
+    ? "https://alumnigrid.com"
     : "http://localhost:3000";
 console.log(IPAddress);
-export const BackendURL = `${IPAddress}`;
+export const BackendURL =
+  process.env.LIVE_MODE === "true"
+    ? "https://backend.alumnigrid.com"
+    : "http://localhost:5000";
 export const FrontendURL = `${IPAddress}`;

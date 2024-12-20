18e2d2f4b95a2b59c7ba4e0c6b867ea22400e14f
bankmotion <harris5555555555555@gmail.com>
2024-12-21 04:03:24 +0900

    UPDATE: api

--- src/controller/playersController.ts
+++ src/controller/playersController.ts
@@ -33,7 +33,7 @@ export const identifyingCollege = async (req: Request, res: Response) => {
   try {
     const { id, college } = req.body;
     const data = await getPlayerDataByID(id);
-    console.log(data)
+    console.log(data);
     if (data?.dataValues.college === college) {
       res.status(200).json({ status: 200, message: true });
     } else {
@@ -44,5 +44,16 @@ export const identifyingCollege = async (req: Request, res: Response) => {
     res
       .status(500)
       .json({ status: 500, message: "Failed to identify college" });
+  }
+};
+
+export const getPlayerInfo = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.body;
+    const data = await getPlayerDataByID(id);
+    res.status(200).json({ status: 200, data });
+  } catch (err) {
+    console.error(`playerController~getPlayerInfo() => ${err}`);
+    res.status(500).json({ status: 500, message: "Failed to get player info" });
   }
 };
